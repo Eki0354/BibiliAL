6e808da454db6e5f6deff16ef28023c69e9b18b3
Eki <ichinoseeki@outlook.com>
2020-12-31 17:28:41 +0800

    加入点赞+两个硬币按钮和点赞+一个硬币+默认收藏按钮

--- js/video/content_scripts.js
+++ js/video/content_scripts.js
@@ -35,62 +35,27 @@ class VideoAL {
       // 添加 点赞+一个币 按钮
       // 投币不可取消，且点赞状态可点击原按钮取消，所以已投币时不予添加显示此按钮
       if (coinState < 2) {
-        this.likeAndOneCoinBtn = document.createElement('span');
-        this.likeAndOneCoinBtn.title = '点赞 + 一个硬币';
-        this.likeAndOneCoinBtn.className = 'al-extra-like';
-        AL.toggle(this.likeAndOneCoinBtn, ON_CLASS_NAME, likeState + coinState >= 4);
-        this.likeAndOneCoinBtn.innerHTML = `
-          <i class="van-icon-videodetails_like like-and-one-coin">
-            <i class="van-icon-videodetails_throw extra-badge"></i>
-          </i>
-        `;
-
-        // 添加事件
-        this.likeAndOneCoinBtn.addEventListener('click', e => {
-          const aid = this.getVideoID();
-          // 1-点赞；2-取消
-          const like = this.getLikeState();
-          VideoAPI.addCoin(aid, 1, like).then(res => {
-            AL.toggle(this.likeAndOneCoinBtn, ON_CLASS_NAME);
-            AL.toggle(this.likeBtn, ON_CLASS_NAME);
-            AL.toggle(this.coinBtn, ON_CLASS_NAME);
-          })
-          // 禁止冒泡触发原来的点赞过程，悬停定位样式需要执行
-          // e.stopPropagation();
-        });
-
+        this.initLikeAndOneCoinBtn(likeState + coinState >= 4);
         ops.appendChild(this.likeAndOneCoinBtn);
       }
 
       // 添加 点赞+默认收藏 按钮
-      this.likeAndFavBtn = document.createElement('span');
-      this.likeAndFavBtn.title = '点赞 + 默认收藏';
-      this.likeAndFavBtn.className = 'al-extra-like';
-      AL.toggle(this.likeAndFavBtn, ON_CLASS_NAME, likeState + favState >= 4);
-      this.likeAndFavBtn.innerHTML = `
-        <i class="van-icon-videodetails_like like-and-fav-default">
-          <i class="van-icon-videodetails_collec extra-badge"></i>
-        </i>
-      `;
-
-      // 添加事件
-      this.likeAndFavBtn.addEventListener('click', async e => {
-        const aid = this.getVideoID();
-        const mid = this.getUpMID();
-        const defaultFav = await this.getDefaultFav(aid, mid);
-        const like = this.getLikeState();
-        const promises = [
-          VideoAPI.like(aid, like),
-          VideoAPI.favToDefault(aid, defaultFav.id, defaultFav.fav_state)
-        ]
-        Promise.all(promises).then(res => {
-          AL.toggle(this.likeAndFavBtn, ON_CLASS_NAME);
-          AL.toggle(this.likeBtn, ON_CLASS_NAME);
-          AL.toggle(this.favBtn, ON_CLASS_NAME);
-        }).catch(e => console.log('请求失败'));
-      });
-
+      this.initLikeAndFavBtn(likeState + favState >= 4);
       ops.appendChild(this.likeAndFavBtn);
+
+      // 添加 点赞+两个币 按钮
+      // 投币不可取消，且点赞状态可点击原按钮取消，所以已投币时不予添加显示此按钮
+      if (coinState < 2) {
+        this.initLikeAndTwoCoinBtn(likeState + coinState >= 4);
+        ops.appendChild(this.likeAndTwoCoinBtn);
+      }
+
+      // 添加 点赞+一个币+默认 按钮
+      // 投币不可取消，且点赞状态可点击原按钮取消，所以已投币时不予添加显示此按钮
+      if (coinState < 2) {
+        this.initLikeAndOneCoinAndFavBtn(likeState + coinState + favState >= 6);
+        ops.appendChild(this.likeAndOneCoinAndFavBtn);
+      }
 
       // 另一套样式，在点赞按钮上添加鼠标停留悬浮模块
       // this.likeBtn = document.querySelector(`#${ARC_TOOLBAR_REPORT} .like`);
@@ -147,6 +112,134 @@ class VideoAL {
   async getDefaultFav (aid, mid) {
     const res = await VideoAPI.getFavList(aid, mid);
     return res.data.list.find(i => i.title === '默认收藏夹');
+  }
+
+  initLikeAndOneCoinBtn (state) {
+    this.likeAndOneCoinBtn = document.createElement('span');
+    this.likeAndOneCoinBtn.title = '点赞 + 一个硬币';
+    this.likeAndOneCoinBtn.className = 'al-extra-like';
+    AL.toggle(this.likeAndOneCoinBtn, ON_CLASS_NAME, state);
+    this.likeAndOneCoinBtn.innerHTML = `
+      <i class="van-icon-videodetails_like like-and-one-coin">
+        <div class="extra-badge-box">
+          <i class="van-icon-videodetails_throw extra-badge"></i>
+        </div>
+      </i>
+    `;
+
+    // 添加事件
+    this.likeAndOneCoinBtn.addEventListener('click', e => {
+      const aid = this.getVideoID();
+      // 1-点赞；2-取消
+      const like = this.getLikeState();
+      VideoAPI.addCoin(aid, 1, like).then(res => {
+        AL.hide(this.likeAndOneCoinBtn);
+        AL.hide(this.likeAndTwoCoinBtn);
+        AL.hide(this.likeAndOneCoinAndFavBtn);
+        AL.toggle(this.likeBtn, ON_CLASS_NAME);
+        AL.toggle(this.coinBtn, ON_CLASS_NAME);
+      });
+      // 禁止冒泡触发原来的点赞过程，悬停定位样式需要执行
+      // e.stopPropagation();
+    });
+  }
+
+  initLikeAndTwoCoinBtn (state) {
+    this.likeAndTwoCoinBtn = document.createElement('span');
+    this.likeAndTwoCoinBtn.title = '点赞 + 两个硬币';
+    this.likeAndTwoCoinBtn.className = 'al-extra-like';
+    AL.toggle(this.likeAndTwoCoinBtn, ON_CLASS_NAME, state);
+    this.likeAndTwoCoinBtn.innerHTML = `
+      <i class="van-icon-videodetails_like like-and-two-coin">
+        <div class="extra-badge-box">
+          <i class="van-icon-videodetails_throw extra-badge"></i>
+          <i class="van-icon-videodetails_throw extra-badge"></i>
+        </div>
+      </i>
+    `;
+
+    // 添加事件
+    this.likeAndTwoCoinBtn.addEventListener('click', e => {
+      const aid = this.getVideoID();
+      // 1-点赞；2-取消
+      const like = this.getLikeState();
+      VideoAPI.addCoin(aid, 2, like).then(res => {
+        AL.hide(this.likeAndOneCoinBtn);
+        AL.hide(this.likeAndTwoCoinBtn);
+        AL.hide(this.likeAndOneCoinAndFavBtn);
+        AL.toggle(this.likeBtn, ON_CLASS_NAME);
+        AL.toggle(this.coinBtn, ON_CLASS_NAME);
+      });
+      // 禁止冒泡触发原来的点赞过程，悬停定位样式需要执行
+      // e.stopPropagation();
+    });
+  }
+
+  initLikeAndFavBtn (state) {
+    this.likeAndFavBtn = document.createElement('span');
+    this.likeAndFavBtn.title = '点赞 + 默认收藏';
+    this.likeAndFavBtn.className = 'al-extra-like';
+    AL.toggle(this.likeAndFavBtn, ON_CLASS_NAME, state);
+    this.likeAndFavBtn.innerHTML = `
+      <i class="van-icon-videodetails_like like-and-fav-default">
+        <div class="extra-badge-box">
+          <i class="van-icon-videodetails_collec extra-badge"></i>
+        </div>
+      </i>
+    `;
+
+    // 添加事件
+    this.likeAndFavBtn.addEventListener('click', async e => {
+      const aid = this.getVideoID();
+      const mid = this.getUpMID();
+      const defaultFav = await this.getDefaultFav(aid, mid);
+      const like = this.getLikeState();
+      const promises = [
+        VideoAPI.like(aid, like),
+        VideoAPI.favToDefault(aid, defaultFav.id, defaultFav.fav_state)
+      ]
+      Promise.all(promises).then(res => {
+        AL.toggle(this.likeAndFavBtn, ON_CLASS_NAME);
+        AL.toggle(this.likeBtn, ON_CLASS_NAME);
+        AL.toggle(this.favBtn, ON_CLASS_NAME);
+      }).catch(e => console.log('请求失败'));
+    });
+  }
+
+  initLikeAndOneCoinAndFavBtn (state) {
+    this.likeAndOneCoinAndFavBtn = document.createElement('span');
+    this.likeAndOneCoinAndFavBtn.title = '点赞 + 一个硬币 + 默认收藏';
+    this.likeAndOneCoinAndFavBtn.className = 'al-extra-like';
+    AL.toggle(this.likeAndOneCoinAndFavBtn, ON_CLASS_NAME, state);
+    this.likeAndOneCoinAndFavBtn.innerHTML = `
+      <i class="van-icon-videodetails_like like-and-one-coin-and-fav-default">
+        <div class="extra-badge-box">
+          <i class="van-icon-videodetails_throw extra-badge"></i>
+          <i class="van-icon-videodetails_collec extra-badge"></i>
+        </div>
+      </i>
+    `;
+
+    // 添加事件
+    this.likeAndOneCoinAndFavBtn.addEventListener('click', async e => {
+      const aid = this.getVideoID();
+      const mid = this.getUpMID();
+      const defaultFav = await this.getDefaultFav(aid, mid);
+      const like = this.getLikeState();
+      const promises = [
+        VideoAPI.addCoin(aid, 1, like),
+        VideoAPI.favToDefault(aid, defaultFav.id, defaultFav.fav_state)
+      ]
+      Promise.all(promises).then(res => {
+        AL.hide(this.likeAndOneCoinBtn);
+        AL.hide(this.likeAndTwoCoinBtn);
+        AL.hide(this.likeAndOneCoinAndFavBtn);
+        AL.toggle(this.likeAndFavBtn, ON_CLASS_NAME);
+        AL.toggle(this.likeBtn, ON_CLASS_NAME);
+        AL.toggle(this.coinBtn, ON_CLASS_NAME);
+        AL.toggle(this.favBtn, ON_CLASS_NAME);
+      }).catch(e => console.log('请求失败'));
+    });
   }
 
   // postFormData (url, data) {
